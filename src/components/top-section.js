826805bc67b1d0b3e75f5f1c5147422a21de3252
matt2112 <matt_lewis@live.co.uk>
2017-01-23 21:46:44 +0000

    get image loading working

--- src/components/top-section.js
+++ src/components/top-section.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-import beach from '../assets/logos/github.png';
-// Jpeg loading seems to be broken, need to find a workaround...
-// import beach from '../assets/bg/beach.jpeg';
+import beach from '../assets/bg/beach.jpeg';
 
 const Top = () => {
     return (
         <div className="top">
-            <img src={beach} />
+            <picture>
+                <source srcSet={beach} media="(min-width: 1380px)" />
+                <source srcSet={beach} media="(min-width: 990px)" />
+                <source srcSet={beach} media="(min-width: 640px)" />
+                <img srcSet={beach} alt="Pretty hi-res image" />
+            </picture>
             <div className="top__band">
                 <h1 className="top__headline">Hi, I'm Matt, a Front End Web Developer based in London, UK.</h1>
             </div>
