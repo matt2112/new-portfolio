0ac9e236e50b15f0a186e1a261d33c80d8ae08bc
matt2112 <matt_lewis@live.co.uk>
2017-01-28 20:59:13 +0000

    add card and various tweaks

---
+++ src/components/card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div className="card">
+            <img sr="" />
+            <h2>Title</h2>
+            <p>Description</p>
+            <button>Button</button>
+        </div>
+    );
+};
+
+export default Card;
--- src/components/featured-projects.js
+++ src/components/featured-projects.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import Card from './card';
+
 const Top = () => {
     return (
         <div className="featured">
             <h1>Featured projects</h1>
+            <h2>Click on the images below to see some of my favourite projects.</h2>
+            <Card />
         </div>
     );
 };
